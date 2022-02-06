bf947401cdd2dc8d8575c5368bb2db9cf1f85d44
John Kim <cloudiosx@gmail.com>
2022-02-06 14:57:18 -0500

    Add complete constraints to the cluesLabel and answersLabel

--- Project8 - 7 Swifty Words/ViewController.swift
+++ Project8 - 7 Swifty Words/ViewController.swift
@@ -18,6 +18,42 @@ class ViewController: UIViewController {
     override func loadView() {
         view = UIView()
         view.backgroundColor = .white
+        
+        scoreLabel = UILabel()
+        scoreLabel.translatesAutoresizingMaskIntoConstraints = false // https://stackoverflow.com/questions/47800210/when-should-translatesautoresizingmaskintoconstraints-be-set-to-true/47801753
+        scoreLabel.textAlignment = .right
+        scoreLabel.text = "Score: 0"
+        view.addSubview(scoreLabel)
+        
+        cluesLabel = UILabel()
+        cluesLabel.translatesAutoresizingMaskIntoConstraints = false
+        cluesLabel.font = UIFont.systemFont(ofSize: 24)
+        cluesLabel.text = "CLUES"
+        cluesLabel.numberOfLines = 0
+        view.addSubview(cluesLabel)
+        
+        answersLabel = UILabel()
+        answersLabel.translatesAutoresizingMaskIntoConstraints = false
+        answersLabel.font = UIFont.systemFont(ofSize: 24)
+        answersLabel.text = "ANSWERS"
+        answersLabel.numberOfLines = 0
+        answersLabel.textAlignment = .right
+        view.addSubview(answersLabel)
+        
+        NSLayoutConstraint.activate([
+            scoreLabel.topAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor),
+            scoreLabel.trailingAnchor.constraint(equalTo: view.layoutMarginsGuide.trailingAnchor, constant: 0),
+            cluesLabel.topAnchor.constraint(equalTo: scoreLabel.bottomAnchor), // pin the top of the clues label to the bottom of the score label
+            cluesLabel.leadingAnchor.constraint(equalTo: view.layoutMarginsGuide.leadingAnchor, constant: 100), // pin the leading edge of the clues label to the leading edge of our layout margins, adding 100 for some space
+            cluesLabel.widthAnchor.constraint(equalTo: view.layoutMarginsGuide.widthAnchor, multiplier: 0.6, constant: -100), // make the clues label 60% of the width of our layout margins, minus 100
+            answersLabel.topAnchor.constraint(equalTo: scoreLabel.bottomAnchor), // also pin the top of the answers label to the bottom of the score label
+            answersLabel.trailingAnchor.constraint(equalTo: view.layoutMarginsGuide.trailingAnchor, constant: -100), // make the answers label stick to the trailing edge of our layout margins, minus 100
+            answersLabel.widthAnchor.constraint(equalTo: view.layoutMarginsGuide.widthAnchor, multiplier: 0.4, constant: -100), // make the answers label take up 40% of the available space, minus 100
+            answersLabel.heightAnchor.constraint(equalTo: cluesLabel.heightAnchor), // make the answers label match the height of the clues label
+        ])
+        
+        cluesLabel.backgroundColor = .red
+        answersLabel.backgroundColor = .blue
     }
 
     override func viewDidLoad() {
@@ -25,4 +61,5 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 }
+
 
